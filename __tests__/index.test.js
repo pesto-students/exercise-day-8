39408682619146da6af942514956b033ade69d64
Anshuman <4nshu19@gmail.com>
2018-05-26 00:44:02 +0530

    Add 15 questions to segment-2 exercises

---
+++ __tests__/index.test.js
@@ -0,0 +1,336 @@
+const {
+  searchSortedMatrix,
+  slice,
+  splitEvery,
+  cipher,
+  kungfoo,
+  sumable,
+  theatre,
+  tiles,
+  toPairs,
+  toUpper1,
+  type,
+  uncurryN,
+  union,
+  uniq,
+  unnest,
+} = require('../src/index');
+
+describe('searchSortedMatrix', () => {
+  test('should implement the function correctly', () => {
+    expect(searchSortedMatrix({
+      search: 18,
+      matrix: [
+        [12, 18, 34, 35],
+        [38, 50, 52, 70],
+        [82, 84, 90, 95],
+      ],
+    })).toBe(true);
+    expect(searchSortedMatrix({
+      search: 41,
+      matrix: [[12, 18, 34, 35, 41, 44]],
+    })).toBe(true);
+    expect(searchSortedMatrix({
+      search: 40,
+      matrix: [[12, 18, 34, 35, 41, 44]],
+    })).toBe(false);
+    expect(searchSortedMatrix({
+      search: 54,
+      matrix: [[1, 2, 3, 4, 5, 6, 9, 10],
+        [12, 14, 15, 17, 18, 20, 22, 23],
+        [24, 25, 26, 27, 28, 34, 35, 36],
+        [37, 42, 43, 44, 45, 46, 48, 49],
+        [50, 51, 52, 56, 61, 62, 63, 64],
+        [65, 69, 70, 75, 76, 80, 81, 82],
+        [83, 84, 90, 82, 84, 90, 95, 96]],
+    })).toBe(false);
+    expect(searchSortedMatrix({
+      search: 80,
+      matrix: [[1, 2, 3, 4, 5, 6, 9, 10],
+        [12, 14, 15, 17, 18, 20, 22, 23],
+        [24, 25, 26, 27, 28, 34, 35, 36],
+        [37, 42, 43, 44, 45, 46, 48, 49],
+        [50, 51, 52, 56, 61, 62, 63, 64],
+        [65, 69, 70, 75, 76, 80, 81, 82],
+        [83, 84, 90, 82, 84, 90, 95, 96]],
+    })).toBe(true);
+  });
+});
+
+describe('slice', () => {
+  it('retrieves the proper sublist of a list', () => {
+    const list = [8, 6, 7, 5, 3, 0, 9];
+    expect(slice(2, 5, list)).toBe([7, 5, 3]);
+  });
+
+  it('can operate on strings', () => {
+    expect(slice(0, 0, 'abc')).toBe('');
+    expect(slice(0, 1, 'abc')).toBe('a');
+    expect(slice(0, 2, 'abc')).toBe('ab');
+    expect(slice(0, 3, 'abc')).toBe('abc');
+    expect(slice(0, 4, 'abc')).toBe('abc');
+    expect(slice(1, 0, 'abc')).toBe('');
+    expect(slice(1, 1, 'abc')).toBe('');
+    expect(slice(1, 2, 'abc')).toBe('b');
+    expect(slice(1, 3, 'abc')).toBe('bc');
+    expect(slice(1, 4, 'abc')).toBe('bc');
+    expect(slice(0, -4, 'abc')).toBe('');
+    expect(slice(0, -3, 'abc')).toBe('');
+    expect(slice(0, -2, 'abc')).toBe('a');
+    expect(slice(0, -1, 'abc')).toBe('ab');
+    expect(slice(0, -0, 'abc')).toBe('');
+    expect(slice(-2, -4, 'abc')).toBe('');
+    expect(slice(-2, -3, 'abc')).toBe('');
+    expect(slice(-2, -2, 'abc')).toBe('');
+    expect(slice(-2, -1, 'abc')).toBe('b');
+    expect(slice(-2, -0, 'abc')).toBe('');
+  });
+});
+
+describe('splitEvery', () => {
+  it('splits a collection into slices of the specified length', () => {
+    expect(splitEvery(1, [1, 2, 3, 4])).toEqual([[1], [2], [3], [4]]);
+    expect(splitEvery(2, [1, 2, 3, 4])).toEqual([[1, 2], [3, 4]]);
+    expect(splitEvery(3, [1, 2, 3, 4])).toEqual([[1, 2, 3], [4]]);
+    expect(splitEvery(4, [1, 2, 3, 4])).toEqual([[1, 2, 3, 4]]);
+    expect(splitEvery(5, [1, 2, 3, 4])).toEqual([[1, 2, 3, 4]]);
+    expect(splitEvery(3, [])).toEqual([]);
+    expect(splitEvery(1, 'abcd')).toEqual(['a', 'b', 'c', 'd']);
+    expect(splitEvery(2, 'abcd')).toEqual(['ab', 'cd']);
+    expect(splitEvery(3, 'abcd')).toEqual(['abc', 'd']);
+    expect(splitEvery(4, 'abcd')).toEqual(['abcd']);
+    expect(splitEvery(5, 'abcd')).toEqual(['abcd']);
+    expect(splitEvery(3, '')).toEqual([]);
+  });
+
+  it('throws if first argument is not positive', () => {
+    expect(() => { splitEvery(0, []); }).toThrow();
+    expect(() => { splitEvery(0, ''); }).toThrow();
+    expect(() => { splitEvery(-1, []); }).toThrow();
+    expect(() => { splitEvery(-1, ''); }).toThrow();
+  });
+});
+
+describe('substitution-cipher', () => {
+  test('should return correct output for an input', () => {
+    expect(cipher('Aa Bb Cc Xx Yy Zz !?.\n')).toBe('Ee Hh Gg Dd Cc Ff !?.\n');
+    expect(cipher('The Quick Brown Fox Jumped Over The Lazy Dog!')).toBe('Zni Uymgo Hxsat Lsd Pyqvij Sbix Zni Refc Jsk!');
+    expect(cipher('Lorem ipsum dolor sit amet, consectetur adipiscing elit! Sed sit amet tortor id turpis pharetra ultricies eget ac ipsum.\nNam porttitor, ligula vitae tincidunt lacinia, lacus eros tempus neque, ac tincidunt nisi dui ac dui. Ut volutpat mi non velit varius, at scelerisque lorem efficitur. Etiam vitae molestie nibh. Integer quis arcu non tortor auctor viverra et at leo?\nSed lacinia nunc non est tincidunt, vel pellentesque magna finibus. Nullam sed lectus sed massa convallis posuere id in augue. Mauris pharetra felis erat, vitae condimentum nisi bibendum id. Vestibulum vitae neque a lectus gravida rhoncus lobortis id elit. Maecenas semper.'))
+      .toBe('Rsxiq mvwyq jsrsx wmz eqiz, gstwigzizyx ejmvmwgmtk irmz! Wij wmz eqiz zsxzsx mj zyxvmw vnexizxe yrzxmgmiw ikiz eg mvwyq.\nTeq vsxzzmzsx, rmkyre bmzei zmtgmjytz regmtme, regyw ixsw ziqvyw tiuyi, eg zmtgmjytz tmwm jym eg jym. Yz bsryzvez qm tst birmz bexmyw, ez wgirixmwuyi rsxiq illmgmzyx. Izmeq bmzei qsriwzmi tmhn. Mtzikix uymw exgy tst zsxzsx eygzsx bmbixxe iz ez ris?\nWij regmtme tytg tst iwz zmtgmjytz, bir virritziwuyi qekte lmtmhyw. Tyrreq wij rigzyw wij qewwe gstberrmw vswyixi mj mt eykyi. Qeyxmw vnexizxe lirmw ixez, bmzei gstjmqitzyq tmwm hmhitjyq mj. Biwzmhyryq bmzei tiuyi e rigzyw kxebmje xnstgyw rshsxzmw mj irmz. Qeigitew wiqvix.');
+  });
+});
+
+describe('sudan function', () => {
+  test('should implement sudan function correctly', () => {
+    expect(kungfoo({ n: 0, x: 5, y: 8 })).toEqual(13);
+    expect(kungfoo({ n: 1, x: 3, y: 4 })).toEqual(74);
+    expect(kungfoo({ n: 2, x: 5, y: 1 })).toEqual(440);
+    expect(kungfoo({ n: 2, x: 0, y: 2 })).toEqual(19);
+  });
+});
+
+describe('sumable', () => {
+  test('should implement sumable as specified', () => {
+    expect(sumable({ target: 8, array: [4, 9, 4, 2] })).toBe(true);
+    expect(sumable({ target: 8, array: [4, 9, 3, 2] })).toBe(false);
+    expect(sumable({ target: 17, array: [4, 9, 6, 7, 4, 2] })).toBe(true);
+    expect(sumable({ target: 27, array: [4, 9, 6, 7, 4, 2] })).toBe(false);
+    expect(sumable({ target: 0, array: [] })).toBe(true);
+    expect(sumable({ target: 1, array: [] })).toBe(false);
+  });
+});
+
+describe('theatre', () => {
+  test('should implement theatre correctly', () => {
+    expect(theatre([10, 10, 20, 10, 10, 10, 50, 10, 20, 50, 10, 20])).toBe(true);
+    expect(theatre([10, 10, 20, 10, 10, 10, 50, 50, 10, 20])).toBe(false);
+    expect(theatre([10, 10, 20, 10, 10, 10, 20, 20, 20, 20])).toBe(true);
+    expect(theatre([10, 10, 10, 20, 10, 10, 50, 10, 20, 10, 20, 20, 20, 10, 50, 20,
+      10, 10, 10, 20, 20, 50, 20])).toBe(true);
+    expect(theatre([10, 10, 10, 20, 10, 10, 50, 20, 10, 20, 20, 20, 10, 50, 20, 10,
+      10, 10, 20, 20, 50, 10, 20])).toBe(false);
+  });
+});
+
+describe('tiles', () => {
+  test('should implement tiles correctly', () => {
+    expect(tiles({ small: 4, big: 3, target: 12 })).toBe(true);
+    expect(tiles({ small: 2, big: 3, target: 18 })).toBe(false);
+    expect(tiles({ small: 8, big: 4, target: 26 })).toBe(true);
+    expect(tiles({ small: 3, big: 3, target: 14 })).toBe(false);
+  });
+});
+
+describe('toPairs', () => {
+  it('converts an object into an array of two-element [key, value] arrays', () => {
+    expect(toPairs({ a: 1, b: 2, c: 3 })).toEqual([['a', 1], ['b', 2], ['c', 3]]);
+  });
+
+  it("only iterates the object's own properties", () => {
+    const F = () => {
+      this.x = 1;
+      this.y = 2;
+    };
+    F.prototype.protoProp = 'you can\'t see me';
+    const f = new F();
+    expect(toPairs(f)).toEqual([['x', 1], ['y', 2]]);
+  });
+});
+
+describe('toUpper', () => {
+  it('returns the upper-case equivalent of the input string', () => {
+    expect(toUpper1('abc')).toBe('ABC');
+  });
+});
+
+describe('type', () => {
+  it('"Array" if given an array literal', () => {
+    expect(type([1, 2, 3])).toBe('Array');
+  });
+
+  // it('"Arguments" if given an arguments object', function() {
+  //   var args = (function() { return arguments; }());
+  //   expect(type(args), 'Arguments');
+  // });
+
+  it('"Object" if given an object literal', () => {
+    expect(type({ batman: 'na na na na na na na' })).toBe('Object');
+  });
+
+  it('"RegExp" if given a RegExp literal', () => {
+    expect(type(/[A-z]/)).toBe('RegExp');
+  });
+
+  it('"Number" if given a numeric value', () => {
+    expect(type(4)).toBe('Number');
+  });
+
+  it('"Number" if given the NaN value', () => {
+    expect(type(NaN)).toBe('Number');
+  });
+
+  it('"String" if given a String literal', () => {
+    expect(type('Gooooodd Mornning Ramda!!')).toBe('String');
+  });
+
+  it('"String" if given a String object', () => {
+    expect(type('I am a String object')).toBe('String');
+  });
+
+  it('"Null" if given the null value', () => {
+    expect(type(null)).toBe('Null');
+  });
+
+  it('"Undefined" if given the undefined value', () => {
+    expect(type(undefined)).toBe('Undefined');
+  });
+});
+
+describe('uncurryN', () => {
+  function a2(a) {
+    return b => a + b;
+  }
+
+  function a3(a) {
+    return b => c => a + b + c;
+  }
+
+  function a3b(a) {
+    return b => (c, ...args) => a + b + c + (args[0] || 0) + (args[1] || 0);
+  }
+
+  function a4(a) {
+    return b => c => d => a + b + c + d;
+  }
+
+  it('accepts an arity', () => {
+    const uncurried = uncurryN(3, a3);
+    expect(uncurried(1, 2, 3)).toBe(6);
+  });
+
+  it('returns a function of the specified arity', () => {
+    expect(uncurryN(2, a2).length).toBe(2);
+    expect(uncurryN(3, a3).length).toBe(3);
+    expect(uncurryN(4, a4).length).toBe(4);
+  });
+
+  it('forwards extra arguments', () => {
+    const g = uncurryN(3, a3b);
+
+    expect(g(1, 2, 3)).toBe(6);
+    expect(g(1, 2, 3, 4)).toBe(10);
+    expect(g(1, 2, 3, 4, 5)).toBe(15);
+  });
+
+  it('works with ordinary uncurried functions', () => {
+    expect(uncurryN(2, (a, b) => a + b)(10, 20)).toBe(30);
+    expect(uncurryN(3, (a, b, c) => a + b + c)(10, 20, 30)).toBe(60);
+  });
+});
+
+describe('union', () => {
+  const M = [1, 2, 3, 4];
+  const N = [3, 4, 5, 6];
+  it('combines two lists into the set of all their elements', () => {
+    expect(union(M, N)).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  it('combines two lists with various elements', () => {
+    expect(union([0], [-0]).length).toEqual(2);
+    expect(union([-0], [0]).length).toEqual(2);
+    expect(union([NaN], [NaN]).length).toEqual(1);
+  });
+});
+
+describe('uniq', () => {
+  it('returns a set from any array (i.e. purges duplicate elements)', () => {
+    const list = [1, 2, 3, 1, 2, 3, 1, 2, 3];
+    expect(uniq(list)).toEqual([1, 2, 3]);
+  });
+
+  it('keeps elements from the left', () => {
+    expect(uniq([1, 2, 3, 4, 1])).toEqual([1, 2, 3, 4]);
+  });
+
+  it('returns an empty array for an empty array', () => {
+    expect(uniq([])).toEqual([]);
+  });
+
+  it('checking length', () => {
+    expect(uniq([-0, -0]).length).toBe(1);
+    expect(uniq([0, -0]).length).toBe(2);
+    expect(uniq([NaN, NaN]).length).toBe(1);
+    expect(uniq([[1], [1]]).length).toBe(1);
+  });
+
+  it('handles null and undefined elements', () => {
+    expect(uniq([undefined, null, undefined, null])).toEqual([undefined, null]);
+  });
+});
+
+describe('unnest', () => {
+  it('only flattens one layer deep of a nested list', () => {
+    let nest = [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10];
+    expect(unnest(nest)).toEqual([1, 2, 3, [4, 5], 6, [[[7], 8]], 9, 10]);
+    nest = [[[[3]], 2, 1], 0, [[-1, -2], -3]];
+    expect(unnest(nest)).toEqual([[[3]], 2, 1, 0, [-1, -2], -3]);
+    expect(unnest([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('is not destructive', () => {
+    const nest = [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10];
+    expect(unnest(nest)).not.toEqual(nest);
+  });
+
+  it('handles array-like objects', () => {
+    const o = {
+      length: 3, 0: [1, 2, [3]], 1: [], 2: ['a', 'b', 'c', ['d', 'e']],
+    };
+    expect(unnest(o)).toEqual([1, 2, [3], 'a', 'b', 'c', ['d', 'e']]);
+  });
+
+  it('flattens an array of empty arrays', () => {
+    expect(unnest([[], [], []])).toEqual([]);
+    expect(unnest([])).toEqual([]);
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,308 @@
+/** Q1.
+ * Removes one level of nesting from
+ * any chain.
+ *
+ *
+ *   unnest([1, [2], [[3]]]); //=> [1, 2, [3]]
+ *   unnest([[1, 2], [3, 4], [5, 6]]); //=> [1, 2, 3, 4, 5, 6]
+ */
+
+function unnest() {
+
+}
+
+
+/** Q2. (*)
+ * Returns a new list containing only one copy of each element in the original
+ * list. [`R.equals`](#equals) is used to determine equality.
+ *
+ *   uniq([1, 1, 2, 1]); //=> [1, 2]
+ *   uniq([1, '1']);     //=> [1, '1']
+ *   uniq([[42], [42]]); //=> [[42]]
+ */
+
+function uniq() {
+
+}
+
+/** Q3. (*)
+ * Combines two lists into a set (i.e. no duplicates) composed of the elements
+ * of each list.
+
+ *      R.union([1, 2, 3], [2, 3, 4]); //=> [1, 2, 3, 4]
+ */
+
+function union() {
+
+}
+
+/** Q4.
+ * Returns a function of arity `n` from a (manually) curried function.
+
+ *      const addFour = a => b => c => d => a + b + c + d;
+ *
+ *      const uncurriedAddFour = uncurryN(4, addFour);
+ *      uncurriedAddFour(1, 2, 3, 4); //=> 10
+ */
+
+function uncurryN() {
+
+}
+
+/** Q5. (*)
+ * Gives a single-word string description of the (native) type of a value,
+ * returning such answers as 'Object', 'Number', 'Array', or 'Null'. Does not
+ * attempt to distinguish user Object types any further, reporting them all as
+ * 'Object'.
+ *
+ *      R.type({}); //=> "Object"
+ *      R.type(1); //=> "Number"
+ *      R.type(false); //=> "Boolean"
+ *      R.type('s'); //=> "String"
+ *      R.type(null); //=> "Null"
+ *      R.type([]); //=> "Array"
+ *      R.type(/[A-z]/); //=> "RegExp"
+ *      R.type(() => {}); //=> "Function"
+ *      R.type(undefined); //=> "Undefined"
+ */
+
+function type() {
+
+}
+
+/** Q6. (*)
+ * The upper case version of a string.
+ *
+ *      R.toUpper('abc'); //=> 'ABC'
+ */
+
+function toUpper1(params) {
+  return params;
+}
+
+/** Q7.
+ * Converts an object into an array of key, value arrays. The object's own
+ * properties and prototype properties are used. Note that the order of the
+ * output array is not guaranteed to be consistent across different JS
+ * platforms.
+ *
+ *      const F = function() { this.x = 'X'; };
+ *      F.prototype.y = 'Y';
+ *      const f = new F();
+ *      toPairsIn(f); //=> [['x','X'], ['y','Y']]
+ */
+
+function toPairs(params) {
+  return params;
+}
+
+
+/** Q8.
+ * Title: Tiles
+ *
+ * Summary: Determine if you can combine small and big tiles to create a specific length.
+ *
+ * Determine if you can create a row of tiles that are a specific
+ * target inches long given a number of small tiles (1 inch
+ * each) and a number of big tiles (5 inches each). Return true
+ * if it is possible else false.
+ *
+ * input: object containing
+ *     small: Integer number of small (1 inch) tiles
+ *       big: Integer number of big (5 inch) tiles
+ *    target: Integer length in inches that we want to create.
+ *
+ * output: true if some combination of small and big tiles will
+ *         create target length, else false.
+ *
+ * Restrictions: Do not use loops (for or while) or recursion.
+ */
+
+function tiles(input) {
+  return input;
+}
+
+/** Q9.
+ * Title: Theatre Line Change
+ *
+ * Summary: Can the theatre box office make change for everyone in the line.
+ *
+ * The theatre box office needs to sell tickets to everyone
+ * in line. I must sell tickets in the order that people are
+ * in the line. A ticket costs $10 and the theatre box office
+ * must give correct change to everyone when they buy their
+ * ticket.
+ *
+ * Everyone in the theatre box office line only has one bill
+ * to pay with: $10, $20, $50.
+ *
+ * Given that the theatre box office starts with no change,
+ * determine if they can sell tickets to everyone in line
+ * and give the correct change at the time each ticket is
+ * purchased.
+ *
+ * input: Array of 10, 20, and 50, representing what bill
+ *        each person in line has.
+ *
+ * output: true if the box office can sell tickets and make
+ *         change for everyone in line, else false.
+ *
+ * For Example:
+ * [10,10,20,10,10,10,50] => true
+ * [20] => false
+ * [10,20,10,20,50] => true
+ * [10,20,20] => false
+ */
+
+function theatre(input) {
+  return input;
+}
+
+/** Q10.
+ * Title: Sumable
+ *
+ * Summary: Given an array of integers, determine if a subset of the array add to a target number.
+ *
+ * Given an array of integers and a target number, is it
+ * possible to choose a set integers, such that the set
+ * sums to the target?
+ *
+ * input: Object containing
+ *     target: integer number
+ *      array: array of integers
+ *
+ * output: true if there exists a set of input array integers
+ *         that add up to input target.
+ *
+ * Restrictions: No loops or helper functions allowed: for, while, function
+ */
+
+function sumable(input) {
+  return input;
+}
+
+/** Q11.
+ * Title: Sudan Function
+ *
+ * Implement Sudan Function defined as
+ *
+ * F(n,x,y) = x+y when n = 0
+ *
+ * F(n,x,y) = x when y = 0 and n >= 0
+ *
+ * F(n,x,y) = F(n-1, F(n,x,y-1), F(n,x,y-1) + y)
+ *            when y > 0 and n >= 0
+ *
+ * Help with Sudan Function:
+ * https://en.wikipedia.org/wiki/Sudan_function
+ *
+ * input: Object containing:
+ *        n: Non-negative Integer
+ *        x: Non-negative Integer
+ *        y: Non-negative Integer
+ *
+ * output: Integer as defined above.
+ */
+
+function kungfoo(input) {
+  return input;
+}
+
+/** Q12. (*)
+ * Title: Substitution Cipher II
+ *
+ * output: String encoded such that:
+ *         A-Z and a-z are replaced with the corresponding letter,
+ *         of the same case, shifted 4 or 6 letters to the right. 'A'
+ *         is shifted 4, 'B' is shifted 6, 'C' is shifted 4, 'D' is
+ *         shifted 6, etc... The same rule applies for the lower case
+ *         letters.
+ *
+ *         If a letter is shifted past Z or z then it wraps
+ *         back around to A or a and continues the remaining
+ *         number of shifted positions.
+ *
+ *         All other characters are left unchanged.
+ *
+ * For Example:
+ * A -> E (shifted right 4 letters)
+ * B -> H (shifted right 6 letters)
+ * Y -> C (shifted right 4 letters)
+ * Z -> F (shifted right 6 letters)
+ * a -> e (shifted right 4 letters)
+ * b -> h (shifted right 6 letters)
+ * y -> c (shifted right 4 letters)
+ * z -> z (shifted right 6 letters)
+ * . -> .
+ * " " -> " " (space)
+ * \n -> \n (newline)
+ */
+
+function cipher(str) {
+  return str;
+}
+
+/** Q13. (*)
+ * Splits a collection into slices of the specified length.
+ *
+ *     splitEvery(3, [1, 2, 3, 4, 5, 6, 7]); //=> [[1, 2, 3], [4, 5, 6], [7]]
+ *     splitEvery(3, 'foobarbaz'); //=> ['foo', 'bar', 'baz']
+ */
+
+function splitEvery(n, list) {
+  return n + list;
+}
+
+
+/** Q14. (*)
+ * Returns the elements of the given list or string (or object with a `slice`
+ * method) from `fromIndex` (inclusive) to `toIndex` (exclusive).
+ *
+ *
+ *    slice(1, 3, ['a', 'b', 'c', 'd']);        //=> ['b', 'c']
+ *    slice(1, Infinity, ['a', 'b', 'c', 'd']); //=> ['b', 'c', 'd']
+ *    slice(0, -1, ['a', 'b', 'c', 'd']);       //=> ['a', 'b', 'c']
+ *    slice(-3, -1, ['a', 'b', 'c', 'd']);      //=> ['b', 'c']
+ *    slice(0, 3, 'ramda');                     //=> 'ram'
+ */
+
+function slice(input) {
+  return input;
+}
+
+
+/** Q15. (*)
+ * Title: Search Sorted Matrix
+ *
+ * input: An object containing:
+ *     'search': an integer to search for.
+ *     'matrix': A 2D array of integers where integers in each row are sorted from
+ *               left to right and the first integer of each row is greater than the
+ *               last integer of the previous row.
+ *
+ * output: return true if 'matrix' contains 'search' else return false.
+ *
+ * The goal is to search as efficiently as possible, fewest statements executed.
+ */
+
+function searchSortedMatrix(input) {
+  return input;
+}
+
+module.exports = {
+  searchSortedMatrix,
+  slice,
+  splitEvery,
+  cipher,
+  kungfoo,
+  sumable,
+  theatre,
+  tiles,
+  toPairs,
+  toUpper1,
+  type,
+  uncurryN,
+  union,
+  uniq,
+  unnest,
+};
