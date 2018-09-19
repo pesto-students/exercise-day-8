f94b03f44e4d86a24e3ac27cd9a1895275ac34ae
vipulrawat <vipulrawat.imi@live.in>
2018-09-20 00:28:41 +0530

    [WIP]

--- src/index.js
+++ src/index.js
@@ -21,7 +21,20 @@
   watchedObject.a.b[0].c = true;
   //=> 'Object changed: 2'
 */
-function onChange() {}
+function onChange() {
+  // eslint-disable-next-line
+  const object = {
+    foo: false,
+    a: {
+      b: [
+        {
+          c: false,
+        },
+      ],
+    },
+  };
+  // let proxy = new Proxy(object, handler);
+}
 
 /* Q2: Use ES6 Proxy to implement the following function
   Call a method on an iterable to call it on all items of the iterable
@@ -60,7 +73,24 @@ const proxyIterable = () => {};
   console.log(obj2.bar);
   //=> [TypeError] Unknown property: bar
 */
-function knownProp() {}
+function knownProp(arg) {
+  const obj = arg;
+  const handler = {
+    get: (target, key) => {
+      if (key in target) {
+        return Reflect.get(target, key);
+      }
+      return new TypeError('Unknown property');
+    },
+    set: (target, key, value) => {
+      // eslint-disable-next-line
+      target[key] = value;
+    },
+  };
+  // eslint-disable-next-line
+  const proxy = new Proxy(obj, handler);
+  return proxy;
+}
 
 /* Q4: Use ES6 Proxy to support negative index in array (*)
 
@@ -69,7 +99,27 @@ function knownProp() {}
   console.log(unicorn[-1]);
   //=> 'rainbow' (gets the 1st element from last)
 */
-function negativeIndex() {}
+function negativeIndex(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError('Only arrays are supported');
+  }
+  const handler = {
+    // eslint-disable-next-line
+    get: (target, key) => {
+      if (key < 0) {
+        return target[Number(3 + Number(key))];
+      }
+      return target[key];
+      // let x = Number(key);
+    },
+    set: (target, key, value) => {
+      // eslint-disable-next-line
+      target[key] = value;
+    },
+  };
+  const proxy = new Proxy(arr, handler);
+  return proxy;
+}
 
 /* Q5: Use ES6 Proxy to get a default property if a non-existing
   property is accessed.
